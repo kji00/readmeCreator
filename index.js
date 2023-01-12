1ed6eb402a9e0e266c3bf145f6d048ba475edbf0
alex.kim <alkkim@gmail.com>
2023-01-12 14:38:09 -0800

    testing .then data upon app initalization

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const generateMarkdown = require('./utils/generateMarkdown.js')
+const fileName = 'README.md'
  
 // TODO: Create an array of questions for user input
 const questions = [
@@ -66,10 +67,19 @@ const questions = [
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => {
+        err ? console.log(err) : console.log('Created README!')
+    });
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+    .then((data) => {
+        console.log(data)
+    })
+}
 
 // Function call to initialize app
 init();
